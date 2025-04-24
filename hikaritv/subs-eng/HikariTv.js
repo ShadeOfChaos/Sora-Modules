3312e383cc0fb4ed2bd97a522dfb6a570d2d7af3
ShadeOfChaos <13075934+ShadeOfChaos@users.noreply.github.com>
2025-04-24 12:32:43 +0200

    Sora v1.0.0

---
+++ hikaritv/subs-eng/HikariTv.js
@@ -0,0 +1,196 @@
+async function searchResults(keyword) {
+    const searchUrl = "https://api.hikari.gg/api/anime/?sort=created_at&order=asc&page=1&search=";
+
+    try {
+        const encodedKeyword = encodeURIComponent(keyword);
+        const response = await soraFetch(searchUrl + encodedKeyword);
+        const json = typeof response === 'object' ? await response.json() : await JSON.parse(response);
+        console.log('JSON: ' + JSON.stringify(json));
+
+        const jsonResults = json.results || [];
+
+        // uid = Hikari internal slug
+        if(jsonResults.length <= 0) throw("No results found");
+        const results = jsonResults.map(result => {
+            return {
+                title: result.ani_name,
+                image: result.ani_poster,
+                href: result.uid + '/',
+            };
+        });
+
+        return JSON.stringify(results);
+    } catch (error) {
+        console.log('soraFetch error: ' + error.message);
+        return JSON.stringify([]);
+    }
+}
+
+async function extractDetails(slug) {
+    const detailsUrl = "https://api.hikari.gg/api/anime/uid/";
+
+    try {
+        const response = await soraFetch(detailsUrl + slug);
+        const json = typeof response === 'object' ? await response.json() : await JSON.parse(response);
+
+        const details = {
+            description: '',
+            aliases: '',
+            airdate: ''
+        }
+
+        let airDate = '';
+        if(json?.ani_aired) {
+            if(json?.ani_aired_fin) {
+                airDate = `${ json.ani_aired } - ${ json.ani_aired_fin }`;
+            } else {
+                airDate = json.ani_aired;
+            }
+        } else {
+            if(json?.ani_release) {
+                airDate = json.ani_release;
+            } else {
+                airDate = 'Aired: Unknown';
+            }
+        }
+
+        details.description = json.ani_synopsis ?? 'No description available';
+        details.aliases = json.ani_synonyms ?? '';
+        details.airdate = airDate;
+
+        return JSON.stringify([details]);
+
+    } catch (error) {
+        console.log('Details error: '  + error.message);
+        return JSON.stringify([{
+            description: 'Error loading description',
+            aliases: '',
+            airdate: 'Aired: Unknown'
+        }]);
+    }
+}
+
+async function extractEpisodes(slug) {
+    const episodesUrl = "https://api.hikari.gg/api/episode/uid/";
+    const embedUrl = "https://api.hikari.gg/api/embed/";
+
+    try {
+        const response = await soraFetch(episodesUrl + slug);
+        const json = typeof response === 'object' ? await response.json() : await JSON.parse(response);
+
+        const episodes = json.map(ep => {
+            return {
+                href: embedUrl + slug + ep.ep_id_name,
+                number: parseInt(ep.ep_id_name),
+                title: ep.ep_name,
+            }
+        });
+
+        return JSON.stringify(episodes);        
+    } catch (error) {
+        console.error('soraFetch error: ' + error.message);
+        return JSON.stringify([]);
+    }
+}
+
+async function extractStreamUrl(url) {
+    const acceptabledProviders = ['Streamwish'];
+
+    try {
+        const response = await soraFetch(url);
+        const json = typeof response === 'object' ? await response.json() : await JSON.parse(response);
+
+        const acceptableStreams = json.filter(stream => acceptabledProviders.includes(stream.embed_name));
+        if(acceptableStreams.length <= 0) throw('No valid streams found');
+        
+        const frameUrl = acceptableStreams[0].embed_frame;
+
+        const streamResponse = await soraFetch(frameUrl);
+        const streamHtml = await streamResponse.text();
+
+        let streamUrl = '';
+
+        if(acceptableStreams[0].embed_name == 'Streamwish') {
+            const streamwishRegex = /links=({[\s\S]+?})/;
+
+            const streamwishPackerRegex = /<script type='text\/javascript'>(eval\(function\(p,a,c,k,e,d\)[\s\S]+?)<\/script>/;
+            const streamwishPacker = streamHtml.match(streamwishPackerRegex);
+            const streamwishUnpacked = unpack(streamwishPacker[1]);
+            
+
+            const files = streamwishUnpacked.match(streamwishRegex);
+            if(!files[1]) {
+                throw('No streams found');
+            }
+
+            const filesJson = JSON.parse(files[1]);
+            
+            if(filesJson.hls2) {
+                return filesJson.hls2;
+            } else if(filesJson.hls4) {
+                return filesJson.hls4;
+            } else {
+                throw('No streams found');
+            }
+        }
+
+        return streamUrl;
+
+    } catch(error) {
+        console.error('soraFetch error: ' + error.message);
+        return null;
+    }
+}
+
+async function soraFetch(url, options = { headers: {}, method: 'GET', body: null }) {
+    try {
+        return await fetchv2(url, options.headers ?? {}, options.method ?? 'GET', options.body ?? null);
+    } catch(e) {
+        try {
+            return await fetch(url, options);
+        } catch(error) {
+            return null;
+        }
+    }
+}
+
+// Adjusted from js-beautify (https://github.com/beautifier/js-beautify/blob/03e3cc02949b42970ab12c2bfbfb33c7bced8eba/js/src/unpackers/p_a_c_k_e_r_unpacker.js)
+function detect(str) {
+    return (get_chunks(str).length > 0);
+}
+
+function get_chunks(str) {
+    var chunks = str.match(/eval\(\(?function\(.*?(,0,\{\}\)\)|split\('\|'\)\)\))($|\n)/g);
+    return chunks ? chunks : [];
+}
+
+function unpack(str) {
+    var chunks = get_chunks(str),
+    chunk;
+    for (var i = 0; i < chunks.length; i++) {
+    chunk = chunks[i].replace(/\n$/, '');
+    str = str.split(chunk).join(unpack_chunk(chunk));
+    }
+    return str;
+}
+
+function unpack_chunk(str) {
+    var unpacked_source = '';
+    var __eval = eval;
+    if (detect(str)) {
+    try {
+        eval = function (s) {
+        unpacked_source += s;
+        return unpacked_source;
+        };
+        __eval(str);
+        if (typeof unpacked_source === 'string' && unpacked_source) {
+        str = unpacked_source;
+        }
+    } catch (e) {
+        // well, it failed. we'll just return the original, instead of crashing on user.
+    }
+    }
+    eval = __eval;
+    return str;
+}
