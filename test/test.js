8678606b6041b2108ab6a8178ee2539b8f844448
ShadeOfChaos <13075934+ShadeOfChaos@users.noreply.github.com>
2025-05-25 13:30:29 +0200

    Zoro subtitles test

---
+++ test/test.js
@@ -0,0 +1,60 @@
+//***** LOCAL TESTING
+const results = await searchResults();
+const details = await extractDetails();
+const episodes = await extractEpisodes();
+const streamUrl = await extractStreamUrl();
+console.log('STREAMURL: ', streamUrl);
+//***** LOCAL TESTING
+
+/**
+ * Searches the website for anime with the given keyword and returns the results
+ * @param {string} keyword The keyword to search for
+ * @returns {Promise<string>} A promise that resolves with a JSON string containing the search results in the format: `[{"title": "Title", "image": "Image URL", "href": "URL"}, ...]`
+ */
+async function searchResults(keyword) {
+    return JSON.stringify([{ title: 'Test show', image: 'https://raw.githubusercontent.com/ShadeOfChaos/Sora-Modules/refs/heads/main/AniCrush/ofchaos.jpg', href: '#' }]);
+}
+
+/**
+ * Extracts the details (description, aliases, airdate) from the given url
+ * @param {string} url The id required to fetch the details
+ * @returns {Promise<string>} A promise that resolves with a JSON string containing the details in the format: `[{"description": "Description", "aliases": "Aliases", "airdate": "Airdate"}]`
+ */
+async function extractDetails(url) {
+    const details = {
+        description: 'Test show',
+        aliases: '',
+        airdate: ''
+    }
+
+    return JSON.stringify([details]);
+}
+
+/**
+ * Extracts the episodes from the given url.
+ * @param {string} url - The id required to fetch the episodes
+ * @returns {Promise<string>} A promise that resolves with a JSON string containing the episodes in the format: `[{ "href": "Episode URL", "number": Episode Number }, ...]`.
+ * If an error occurs during the fetch operation, an empty array is returned in JSON format.
+ */
+async function extractEpisodes(url) {
+    return JSON.stringify([{
+        href: '#',
+        number: 1
+    }]);
+}
+
+/**
+ * Extracts the stream URL from the given url, using a utility function on ac-api.ofchaos.com.
+ * @param {string} url - The url to extract the stream URL from.
+ * @returns {Promise<string|null>} A promise that resolves with the stream URL if successful, or null if an error occurs during the fetch operation.
+ */
+async function extractStreamUrl(url) {
+    try {
+        const streamUrl = { stream: 'https://douvid.xyz/_v1_douvid/NVjXNaxco1crLa45Sa9JYw==/jfxMeLVKvB.m3u8", subtitles: https://s.megastatics.com/subtitle/ae54d67674aea3d25dd054caf4345097/eng-2.vtt' };
+        return streamUrl;
+
+    } catch(e) {
+        console.log('Error extracting stream: ' + e.message);
+        return null;
+    }
+}
