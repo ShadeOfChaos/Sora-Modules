5e41b8b5587412581debcd0c18a8ab846096e0bd
ShadeOfChaos <13075934+ShadeOfChaos@users.noreply.github.com>
2025-06-02 21:01:38 +0200

    Removed validity check as it's failure is crashing Sora

--- Miruro/dev/Miruro.js
+++ Miruro/dev/Miruro.js
@@ -2,16 +2,16 @@ const BASE_URLS = ['https://miruro.tv', 'https://miruro.to', 'https://miruro.onl
 const SEARCH_URL = '---/api/search/browse?search=|||&page=1&perPage=5&type=ANIME&sort=SEARCH_MATCH';
 
 // ***** LOCAL TESTING
-// (async() => {
-//     const results = await searchResults('Solo leveling');
-//     console.log('SEARCH RESULTS: ', results);
-//     const details = await extractDetails(JSON.parse(results)[0].href);
-//     console.log('DETAILS: ', details);
-//     const episodes = await extractEpisodes(JSON.parse(results)[0].href);
-//     console.log('EPISODES: ', episodes);
-//     const streamUrl = await extractStreamUrl(JSON.parse(episodes)[0].href);
-//     console.log('STREAMURL: ', streamUrl);
-// })();
+(async() => {
+    const results = await searchResults('Solo leveling');
+    console.log('SEARCH RESULTS: ', results);
+    const details = await extractDetails(JSON.parse(results)[0].href);
+    console.log('DETAILS: ', details);
+    const episodes = await extractEpisodes(JSON.parse(results)[0].href);
+    console.log('EPISODES: ', episodes);
+    const streamUrl = await extractStreamUrl(JSON.parse(episodes)[0].href);
+    console.log('STREAMURL: ', streamUrl);
+})();
 //***** LOCAL TESTING
 
 
@@ -284,14 +284,14 @@ async function extractStreamUrl(objString) {
         }
 
         // Verify if streamsUrls are valid / have not been removed
-        let validStreams = [];
-        for(let stream of multiStreams.streams) {
-            const response = await soraFetch(stream.streamUrl, { method: 'HEAD', headers: stream.headers });
-            if(response?.status === 200) {
-                validStreams.push(stream);
-            }
-        }
-        multiStreams.streams = validStreams;
+        // let validStreams = [];
+        // for(let stream of multiStreams.streams) {
+        //     const response = await soraFetch(stream.streamUrl, { method: 'HEAD', headers: stream.headers });
+        //     if(response?.status === 200) {
+        //         validStreams.push(stream);
+        //     }
+        // }
+        // multiStreams.streams = validStreams;
 
 
         return JSON.stringify(multiStreams);
