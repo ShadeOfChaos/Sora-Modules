1f91e18bbb6404177016cf56ecc35d896d068dfe
ShadeOfChaos <13075934+ShadeOfChaos@users.noreply.github.com>
2025-06-15 03:34:35 +0200

    ...

---
+++ AniNow/dev/aninow9.js
@@ -0,0 +1,16 @@
+async function searchResults(keyword) {
+    console.log('SEARCHING');
+    return JSON.stringify([]);
+}
+
+async function extractDetails(url) {
+    return JSON.stringify();
+}
+
+async function extractEpisodes(url) {
+    return JSON.stringify([]);
+}
+
+async function extractStreamUrl(url) {
+    return JSON.stringify([]);
+}
