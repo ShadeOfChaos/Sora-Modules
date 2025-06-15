0149f1a52eac6ffd26937aed5d1335bd292ed712
ShadeOfChaos <13075934+ShadeOfChaos@users.noreply.github.com>
2025-06-15 03:02:18 +0200

    ...

--- AniNow/dev/aninow.js
+++ AniNow/dev/aninow.js
@@ -6,20 +6,10 @@
 // console.log('STREAMURL: ', streamUrl);
 //***** LOCAL TESTING
 
-/**
- * Searches the website for anime with the given keyword and returns the results
- * @param {string} keyword The keyword to search for
- * @returns {Promise<string>} A promise that resolves with a JSON string containing the search results in the format: `[{"title": "Title", "image": "Image URL", "href": "URL"}, ...]`
- */
 async function searchResults(keyword) {
     return JSON.stringify([{ title: 'Test show', image: 'https://raw.githubusercontent.com/ShadeOfChaos/Sora-Modules/refs/heads/main/AniCrush/ofchaos.jpg', href: '#' }]);
 }
 
-/**
- * Extracts the details (description, aliases, airdate) from the given url
- * @param {string} url The id required to fetch the details
- * @returns {Promise<string>} A promise that resolves with a JSON string containing the details in the format: `[{"description": "Description", "aliases": "Aliases", "airdate": "Airdate"}]`
- */
 async function extractDetails(url) {
     const details = {
         description: 'Test show',
@@ -30,12 +20,6 @@ async function extractDetails(url) {
     return JSON.stringify([details]);
 }
 
-/**
- * Extracts the episodes from the given url.
- * @param {string} url - The id required to fetch the episodes
- * @returns {Promise<string>} A promise that resolves with a JSON string containing the episodes in the format: `[{ "href": "Episode URL", "number": Episode Number }, ...]`.
- * If an error occurs during the fetch operation, an empty array is returned in JSON format.
- */
 async function extractEpisodes(url) {
     return JSON.stringify([{
         href: '#',
@@ -43,11 +27,6 @@ async function extractEpisodes(url) {
     }]);
 }
 
-/**
- * Extracts the stream URL from the given url, using a utility function on ac-api.ofchaos.com.
- * @param {string} url - The url to extract the stream URL from.
- * @returns {Promise<string|null>} A promise that resolves with the stream URL if successful, or null if an error occurs during the fetch operation.
- */
 async function extractStreamUrl(url) {
     const REGEX = /data-media-sources="([\s\S]*?)"/;
     const apiUrl = 'https://aninow.to/api/{source}/sources?url=';
@@ -82,7 +61,6 @@ async function extractStreamUrl(url) {
             let fetchUrl = null;
             let provider = source.provider.toLowerCase();
 
-            console.log('Provider: ', provider, source.language);
             if(provider == 'pahe') {
                 if(source?.kwikurl != null) {
                     if(source.language.toLowerCase() != 'english') continue;
