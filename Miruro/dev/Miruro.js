21882f0d8e9fd4dcf87b0635d25021353ea23738
ShadeOfChaos <13075934+ShadeOfChaos@users.noreply.github.com>
2025-07-07 12:19:55 +0200

    Testing direct file access for AnimeKai and Zoro streams

--- Miruro/dev/Miruro.js
+++ Miruro/dev/Miruro.js
@@ -470,9 +470,11 @@ async function extractKai(data, json, episodeNr, category = 'sub') {
                 tracks = tracks.filter(track => track.kind === 'captions');
             }
 
-            let url = `https://prxy.miruro.to/m3u8?url=${ source.url }`;
+            // let url = `https://prxy.miruro.to/m3u8?url=${ source.url }`;
+            let url = source.url;
+            const referer = json.host + '/';
 
-            sources.push({ provider: 'animekai', url: url, subtitles: tracks, type: category, referer: json.host, origin: json.host });
+            sources.push({ provider: 'animekai', url: url, subtitles: tracks, type: category/*, referer: referer, origin: json.host*/ });
         }
 
         return sources;
@@ -519,9 +521,11 @@ async function extractZoro(data, json, episodeNr, category = 'sub') {
                 tracks = tracks.filter(track => track.kind === 'captions');
             }
 
-            let url = `https://prxy.miruro.to/m3u8?url=${ source.url }`;
+            // let url = `https://prxy.miruro.to/m3u8?url=${ source.url }`;
+            let url = source.url;
+            const referer = 'https://megacloud.club/';
 
-            sources.push({ provider: 'zoro', url: url, subtitles: tracks, type: category, referer: json.host, origin: json.host });
+            sources.push({ provider: 'zoro', url: url, subtitles: tracks, type: category, referer: referer/*, origin: json.host*/ });
         }
 
         return sources;
